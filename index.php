f8d4fea7606c8a828a9ff729a3b41131b572b722
LucaLF-dev <laficaraluca@gmail.com>
2023-11-28 18:24:26 +0100

    fixed : error

--- index.php
+++ index.php
@@ -1,41 +1,48 @@
 <?php
 
-class Production {
+class Production
+{
 
     public $title;
     public $language;
-    public $rating = 0;
+    public $rating;
 
 
     function __construct($_title, $_language, $_rating)
     {
         $this->title = $_title;
         $this->language = $_language;
-        $this->rating = $_rating;
+        $this->setRating($_rating);
     }
 
-    public function setRating($rating) {
+    public function setRating($rating)
+    {
         if (is_numeric($rating) && $rating >= 0 && $rating <= 10) {
             $this->rating = $rating;
         } else {
-            $this->rating = 0;
+            //'il numero non è compreso tra 0 e 10';
+            $this->rating = null;
         }
+    }
+
+    public function getRating() {
+        return $this->rating;
     }
 
     function getLanguage()
     {
-      return $this->language;
-       if (is_null($this->language)) {
-         return 'unknow';
-       } else {
-         return $this->language;
-       }
+        //return $this->language;
+        if (is_null($this->language)) {
+            return null;
+        } else {
+            return $this->language;
+        }
     }
-}
+};
 
-$production1 = new Production( 'il Signore degli Anelli', 'inglese', '8');
-$production2 = new Production( 'il Signore degli Anelli, il ritorno del re', 'inglese', '8');
-$production3 = new Production( 'il Signore degli Anelli, le due torri ', 'inglese', '7');
+$production1 = new Production('il Signore degli Anelli', 'inglese', '8');
+$production2 = new Production('il Signore degli Anelli, il ritorno del re', 'inglese', '8');
+$production3 = new Production('il Signore degli Anelli, le due torri ', 'italiano', '20');
 
 
 $films = [
@@ -45,15 +52,15 @@ $films = [
 ];
 
 foreach ($films as $film) {
-    ?>
+?>
 
-  <div>
-    <h1><?= $film->title  ?></h1>
-    <p><?=  $film->language ?></p>
-    <p>
-      <?php $film->setRating($rating) ?>
-    </p>
-  </div>
+    <div>
+        <h1><?= $film->title  ?></h1>
+        <p><?= $film->getLanguage() ?? 'Lingua non inserita' ?></p>
+        <p>
+            <?php echo $film->getRating() ?? 'il numero inserito non è compreso tra 0 e 10' ?>
+        </p>
+    </div>
 
 <?php
 }
