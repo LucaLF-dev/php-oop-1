29c43a520021514cf5093245a72428163df9e475
LucaLF-dev <laficaraluca@gmail.com>
2023-11-28 17:41:37 +0100

    Add - class production and function constructure

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+class Production {
+
+    public $title;
+    public $language;
+    public $rating;
+
+
+    function __construct($_title, $_language, $_rating)
+    {
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->rating = $_rating;
+    }
+}
