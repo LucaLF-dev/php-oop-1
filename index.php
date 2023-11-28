a9ece2c8de6f7e2c8a85648fb8b40cce5cc7c2b1
LucaLF-dev <laficaraluca@gmail.com>
2023-11-28 17:50:09 +0100

    Add - public function  to class Production

--- index.php
+++ index.php
@@ -13,4 +13,23 @@ class Production {
         $this->language = $_language;
         $this->rating = $_rating;
     }
+
+    public function setRating($rating) {
+        if (is_numeric($rating) && $rating >= 0 && $rating <= 10) {
+            $this->rating = $rating;
+        } else {
+            $this->rating = 'il numero non è compreso tra 0 e 10';
+            echo $this->rating;
+        }
+    }
+
+    function getLanguage()
+    {
+      return $this->language;
+       if (is_null($this->language)) {
+         return 'unknow';
+       } else {
+         return $this->language;
+       }
+    }
 }
